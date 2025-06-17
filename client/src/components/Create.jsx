0e2304115fc396f68d732701b6365530b8670757
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-17 20:39:56 +0100

    updated components and finished project

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const Create = () => {
-  const [task, setTask] = useState();
+const Create = ({ onAdd }) => {
+  const [task, setTask] = useState('');
+
   const handleAdd = () => {
-    axios.post('http://localhost:5000/add', {task: task})
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
-  }
+    axios.post('http://localhost:5000/add', {task})
+ .then((result) => {
+      onAdd(result.data);
+      setTask(''); 
+    })
+    .catch(err => console.log(err));
+};
 
   return (
     <div className="create_form">
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,44 +1,80 @@
 import React, { useState, useEffect } from "react";
-import Create from '../components/Create'
+import Create from "../components/Create";
 import axios from "axios";
-import { BsCircleFill, BsFillTrashFill } from "react-icons/bs";
-
+import {
+  BsCircleFill,
+  BsFillCheckCircleFill,
+  BsFillTrashFill,
+} from "react-icons/bs";
 
 const Home = () => {
-    const [ todos, setTodos ] = useState([])
+  const [todos, setTodos] = useState([]);
 
-    useEffect(() => {
-        axios.get('http://localhost:5000/get')
-        .then(result => setTodos(result.data))
-        .catch(err => console.log(err))
-    }, [])
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/get")
+      .then((result) => setTodos(result.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-    const handleEdit = () => {
-        
-    }
+   const handleAdd = (newTodo) => {
+    setTodos(prev => [...prev, newTodo]);
+  };
 
-    return (
-        <div className="home">
-            <h2> To Do App </h2>
-            <Create />
-            {
-                todos.length === 0 ?
-                    <div> <h2> No Record</h2> </div>
-                    :
-                todos.map(todo => (
-                    <div className='task' key={todo._id}> 
-                        <div className='checkbox' onClick={handleEdit}> 
-                            <BsCircleFill className='icon'/>
-                    <p> {todo.task} </p>
-                     </div>
-                     <div>
-                        <span> <BsFillTrashFill className='icon' /> </span>
-                     </div>
-                     </div>
-                ))
-            }
+  const handleEdit = (id) => {
+    axios
+      .put(`http://localhost:5000/update/${id}`, { done: true })
+   .then((result) => {
+      setTodos(prevTodos =>
+        prevTodos.map(todo => (todo._id === id ? result.data : todo))
+      );
+    })
+    .catch(err => console.log(err));
+};
+
+const handleDelete = (id) => {
+  axios.delete(`http://localhost:5000/delete/${id}`)
+    .then(() => {
+      setTodos(prevTodos => prevTodos.filter(todo => todo._id !== id));
+    })
+    .catch(err => console.log(err));
+};
+
+  return (
+    <div className="home">
+      <h2> To Do App </h2>
+      <Create onAdd= {handleAdd}/>
+      {todos.length === 0 ? (
+        <div>
+          <h2> No Record</h2>
         </div>
-    )
-}
+      ) : (
+        todos.map((todo) => (
+          <div className="task" key={todo._id}>
+            <div className="checkbox" onClick={() => handleEdit(todo._id)}>
+              {todo.done ? (
+                <BsFillCheckCircleFill className="icon">
+                  {" "}
+                </BsFillCheckCircleFill>
+              ) : (
+                <BsCircleFill className="icon" />
+              )}
+              <p className={todo.done ? "line_through" : ""}> {todo.task}</p>
+            </div>
+
+            <div>
+              <span>
+                <BsFillTrashFill
+                  className="icon"
+                  onClick={() => handleDelete(todo._id)}
+                />
+              </span>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
 
 export default Home;
