e9d48dbbc87989866a8edae03643b14aa5c188a2
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-17 20:41:36 +0100

    updated TodoModel and added controller functionality in server.js

--- server/models/TodoModel.js
+++ server/models/TodoModel.js
@@ -4,9 +4,12 @@ const TodoSchema = new mongoose.Schema({
   task: {
     type: String,
     required: true
+  },
+    done: {
+      type: Boolean,
+      default: false
   }
-});
+})
 
 const TodoModel = mongoose.model("todos", TodoSchema);
-
 module.exports = TodoModel;
--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const express = require("express");
 // const mongoose = require('mongoose')
 const cors = require("cors");
 const PORT = 5000 || process.env.PORT;
-const connectDB = require('./database/connectDB')
-const TodoModel = require('./models/TodoModel')
+const connectDB = require("./database/connectDB");
+const TodoModel = require("./models/TodoModel");
 
 // create server
 const app = express();
@@ -14,26 +14,59 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/home', (req, res) => {
-    res.send('Home Page')
-})
+// routes
+app.get("/home", (req, res) => {
+  res.send("Home Page");
+});
 
-app.get('/get', async (req, res) => {
-    TodoModel.find()
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+app.get("/get", async (req, res) => {
+  TodoModel.find()
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
 app.post("/add", async (req, res) => {
-  const task = req.body.task;
+  const { task } = req.body;
   await TodoModel.create({
-    task: task
-  }).then(result => res.json(result))
-  .catch(err => res.json(err))
+    task,
+  })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+app.put("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { task, done } = req.body;
+
+    const updatedFields = {};
+    if (task !== undefined) updatedFields.task = task;
+    if (done !== undefined) updatedFields.done = done;
+
+    const updatedTodo = await TodoModel.findByIdAndUpdate(
+      req.params.id,
+      { $set: updatedFields },
+      { new: true }
+    );
+
+    res.json(updatedTodo);
+  } catch (err) {
+    console.error("Update Error:", err);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: err.message });
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await TodoModel.findByIdAndDelete({ _id: id }, { done: true })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
 });
 
 // start server
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}!`);
-  await connectDB()
+  await connectDB();
 });
