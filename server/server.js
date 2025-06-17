d38892dda10b4c8181536a00026f8aa7c86ac77f
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-17 11:57:07 +0100

    added TodoModel with schema, followed by router and controller folders

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const PORT = 5000 || process.env.PORT;
 const connectDB = require('./database/connectDB')
+const TodoModel = require('./models/TodoModel')
 
 // create server
 const app = express();
@@ -13,12 +14,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("add", (req, res) => {
+app.get('/home', (req, res) => {
+    res.send('Home Page')
+})
+
+app.get('/get', async (req, res) => {
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.post("/add", async (req, res) => {
   const task = req.body.task;
+  await TodoModel.create({
+    task: task
+  }).then(result => res.json(result))
+  .catch(err => res.json(err))
 });
 
 // start server
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}!`);
-  connectDB()
+  await connectDB()
 });
